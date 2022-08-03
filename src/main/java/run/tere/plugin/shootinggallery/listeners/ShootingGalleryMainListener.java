d30019da4f08023f2f6a1a8463974fffc51895c9
tererun <27857171+tererun@users.noreply.github.com>
2022-08-04 01:40:09 +0900

    :construction: WIP (8)

--- src/main/java/run/tere/plugin/shootinggallery/listeners/ShootingGalleryMainListener.java
+++ src/main/java/run/tere/plugin/shootinggallery/listeners/ShootingGalleryMainListener.java
@@ -1,18 +1,10 @@
 package run.tere.plugin.shootinggallery.listeners;
 
-import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.inventory.ItemStack;
-import run.tere.plugin.shootinggallery.ShootingGallery;
-import run.tere.plugin.shootinggallery.defines.GameStall;
-import run.tere.plugin.shootinggallery.defines.GameStallStatus;
-import run.tere.plugin.shootinggallery.handlers.GameStallStatusHandler;
-import run.tere.plugin.shootinggallery.utils.ChatUtil;
 import run.tere.plugin.shootinggallery.utils.GameStallUtil;
 
 import java.util.UUID;
@@ -22,14 +14,10 @@ public class ShootingGalleryMainListener implements Listener {
     @EventHandler
     public void onPlayerInteractEntity(PlayerInteractEntityEvent e) {
         Player player = e.getPlayer();
-
         Entity rightClickedEntity = e.getRightClicked();
         UUID gameStallUUID = GameStallUtil.getGameStallUUID(rightClickedEntity);
-        GameStallStatusHandler gameStallStatusHandler = ShootingGallery.getInstance().getGameStallStatusHandler();
         if (gameStallUUID == null) return;
-        GameStallStatus gameStallStatus = gameStallStatusHandler.getGameStallStatus(gameStallUUID);
-        if (gameStallStatus == null) return;
-
+        GameStallUtil.buy(player);
     }
 
 }
